997b1ec91d2d86417fe75360da132a12bc0b3b07
Pavel Rosolko <rsk.pawel@gmail.com>
2022-07-14 09:26:26 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskOneComponent } from "./task-one/task-one.component";
+import { TaskTwoComponent } from "./task-two/task-two.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'/application',
+    component: TaskOneComponent
+  },
+  {
+    path:'/two',
+    component: TaskTwoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskOneComponent } from './task-one/task-one.component';
+import { TaskTwoComponent } from './task-two/task-two.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskOneComponent
+    TaskOneComponent,
+    TaskTwoComponent
   ],
   imports: [
     BrowserModule,
