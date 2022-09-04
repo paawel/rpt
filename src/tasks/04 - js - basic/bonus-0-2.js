812dc9bf9280956b0e7be2d32df7abaeb33b73c5
Pavel Rosolko <rsk.pawel@gmail.com>
2022-09-04 18:12:44 +0300

    update

--- src/tasks/04 - js - basic/bonus-0-2.js
+++ src/tasks/04 - js - basic/bonus-0-2.js
@@ -1,7 +1,13 @@
-const arr = [1, 2, 3, 4];
+// input:
+let myArrCollection = [
+  [0,  1,  2,  3,  4 ,
+    [5,  6,  7,  8,  9,
+      [10, 11, 12, 13, 14,
+        [15, 16, 17, 18, 19]
+      ]
+    ]
+  ],
+];
 
-for (var i = 0; i < arr.length; i++) {
-  setTimeout(function() {
-    console.log('Index: ' + i + ', element: ' + arr[i]);
-  }, 1000);
-}
+// output:
+// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
